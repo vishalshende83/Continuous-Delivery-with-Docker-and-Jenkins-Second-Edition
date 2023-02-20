402c270e3d2c519ca1afce16f60798ac43389b9b
Vishal Shende <vishal.shende@gmail.com>
2023-02-19 21:01:32 -0500

    Added comments to Calculator.java

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -3,6 +3,7 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+// Adding comments
 @Service
 public class Calculator {
         final static int umlNUMBER1 = 3;
